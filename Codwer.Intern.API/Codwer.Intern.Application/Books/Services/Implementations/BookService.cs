45852eb2154f9e9cec9e578339de4646fb5ffdcf
DESKTOP-QKDPLSL\CWR-Anastasia-C <anastasia.cabac@codwer.com>
2021-11-01 16:51:33 +0200

    controller

---
+++ Codwer.Intern.API/Codwer.Intern.Application/Books/Services/Implementations/BookService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Codwer.Intern.Persistence.Context;
+
+namespace Codwer.Intern.Application.Books.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+
+        private readonly AppDbContext appDbContext;
+        public Task<List<string>> GetBooksName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Codwer.Intern.API/Codwer.Intern/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Codwer.Intern.Application.Books.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codwer.Intern.Controllers
+{
+    [Route("api/[controller]")]
+    class BookController : Controller
+    {
+        private readonly IBookService _bookService;
+        public BookController(IBookService bookService) 
+        {
+            _bookService = bookService;
+        }
+
+
+        [HttpGet("books-name")]
+        public async Task<List<string>> GetApplicationToken()
+        {
+            return await _bookService.GetBooksName();
+        }
+    }
+}
