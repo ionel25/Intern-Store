2ae61e7ab05c53d780523cf041603e3c0cedb421
Victor Banari <victor.banari56@gmail.com>
2021-11-01 14:28:55 +0200

    resolve conflicts

--- Codwer.Intern.API/Codwer.Intern.Persistence/Context/AppDbContext.cs
+++ Codwer.Intern.API/Codwer.Intern.Persistence/Context/AppDbContext.cs
@@ -7,27 +7,19 @@ namespace Codwer.Intern.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-<<<<<<< HEAD
             optionsBuilder.UseSqlServer(@"Server=localhost;Database=localDatabase;Trusted_Connection = True;");
-=======
-            optionsBuilder.UseSqlServer(@"Server=localhost ;Database=localDatabase;Trusted_Connection = True;");
->>>>>>> 657f03c61e2ea4dbdda2e9f7500da989e0947487
         }
 
         public AppDbContext()
         {
         }
         public virtual DbSet<Book> Books { get; set; }
-<<<<<<< HEAD
         public virtual DbSet<Author> Author { get; set; }
         public virtual DbSet<Edition> Edition { get; set; }
         public virtual DbSet<Type> Type { get; set; }
-
-=======
         public virtual DbSet<Cover> Covers { get; set; }
         public virtual DbSet<Language> Languages { get; set; }
         public virtual DbSet<PartnerPrice> PartnerPrices  { get; set; }
->>>>>>> 657f03c61e2ea4dbdda2e9f7500da989e0947487
     }
 
 }
--- Codwer.Intern.API/Codwer.Intern.Persistence/Entities/Book.cs
+++ Codwer.Intern.API/Codwer.Intern.Persistence/Entities/Book.cs
@@ -6,23 +6,16 @@ namespace Codwer.Intern.Persistence.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-<<<<<<< HEAD
-        
         public DateTime Year { get; set; }
-
         public int AuthorId { get; set; }
         public Author Author { get; set; }
         public int EditionId { get; set; }
         public Edition Edition { get; set; }
-
-
-=======
         public int IDCover { get; set; }
         public int IDPartnerPrice { get; set; }
         public int IDLanguage { get; set; }
         public Cover Cover { get; set; }
         public PartnerPrice  PartnerPrice { get; set; }
         public Language Language { get; set; }
->>>>>>> 657f03c61e2ea4dbdda2e9f7500da989e0947487
     }
 }
